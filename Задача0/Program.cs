1376db62d73aaa62ea579e4169dad9bd668daed1
Konstantin <kzhestovskih2012@gmail.com>
2022-09-20 22:36:40 +0500

    задача 3

--- Задача0/Program.cs
+++ Задача0/Program.cs
@@ -3,5 +3,5 @@ Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 int sqr = number * number;
 Console.WriteLine($"Квадрат числа {number} равняется {sqr}");
-int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+int sqr1 = Convert.ToInt32(Math.Pow(number, 10));
 Console.WriteLine($"Квадрат числа {number} равняется {sqr1}");
---
+++ Задача1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int c = b * b;
+
+if (c == a)
+{
+    Console.WriteLine($"первое число {a} является квадратом {b}");
+}
+else 
+{
+    Console.WriteLine($"первое число {a} не является квадратом {b}");
+}
+
