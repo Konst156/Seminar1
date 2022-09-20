8644518270fddd99acbae6d0c538ff962b09b76a
Konstantin <kzhestovskih2012@gmail.com>
2022-09-21 00:09:35 +0500

    улучшил задачу 7

--- Задача7/Program.cs
+++ Задача7/Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите трехзначное число: ");
 int N = int.Parse(Console.ReadLine());
-Console.Write(N % 10);
+
+if (N < 100 || N > 999) {
+    Console.Write("Вы ввели не трехзначное число");
+    return;
+} else {
+    Console.Write(N % 10);
+}
