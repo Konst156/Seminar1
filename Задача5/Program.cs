4de12a9beade1f88c12a83998e4364c4da647eb9
Konstantin <kzhestovskih2012@gmail.com>
2022-09-20 23:45:02 +0500

    Добавил задачу 5

---
+++ Задача5/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N > 0) {
+    int b = N * (-1);
+    while(b <= N)
+    {
+        Console.Write($"{b} ");
+        b++;
+    }
+} else {
+    int b = N * (-1);
+    while(b >= N)
+    {
+        Console.Write($"{N} ");
+        N++;
+    }
+}
